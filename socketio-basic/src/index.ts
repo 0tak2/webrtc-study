58916f97a5d718dd01254fec8c3e216fc1f3bed0
0tak2 <0tak2.code@gmail.com>
2023-10-08 23:06:39 +0900

    간단한 채팅 예제 향상
    - 닉네임 지원
    - 이벤트 분리

--- socketio-basic/src/index.ts
+++ socketio-basic/src/index.ts
@@ -3,11 +3,22 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import path from 'path';
 
+interface UserInfo {
+  id: string;
+  nickname: string;
+  joinDt: Date;
+}
+
 const app = express();
 const server = createServer(app);
 const port = 5000;
 const io = new Server(server);
 const dir = path.resolve();
+let users: UserInfo[] = [];
+
+function findUserBySocketId(socketId: string) {
+  return users.find((el) => el.id === socketId);
+}
 
 app.get('/test', (req, res) => {
   console.log("Served '/test'");
@@ -19,17 +30,34 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected: ', socket.id);
 
-  socket.broadcast.emit('chat message', '새로운 유저가 접속했습니다.');
-  socket.emit('chat message', '채팅에 오신 것을 환영합니다.');
+  socket.emit('system', '채팅에 오신 것을 환영합니다.');
 
-  socket.on('chat message', (msg) => {
-    console.log(`message: ${msg}`);
-    io.emit('chat message', msg);
+  socket.on('join', (nickname) => {
+    const newUser = {
+      id: socket.id,
+      nickname,
+      joinDt: new Date(),
+    };
+
+    users.push(newUser);
+
+    socket.broadcast.emit('join', {
+      new: newUser,
+      users,
+    });
+  });
+
+  socket.on('chat message', (message) => {
+    io.emit('chat message', {
+      message,
+      from: findUserBySocketId(socket.id),
+    });
   });
 
   socket.on('disconnect', () => {
+    users = users.filter((el) => el.id !== socket.id);
     console.log('user disconnected');
   });
 });
